685847d4e60f570b6cb7037e5307bfc32b80715c
suhaibokour <okoursuhaib@gmail.com>
2021-09-17 22:24:26 +0300

    SuhaibFrontEnd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Suhaib from './suhaibComponents/suhaibMain'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>HEllO</h1>
+        <Suhaib/>
       </div>
+      
     );
   }
 }
