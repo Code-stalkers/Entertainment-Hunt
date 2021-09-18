6b4ca763d893f61dcd3fc1fc15da7268dd92d807
Yousef Obeidat <y.linux96@gmail.com>
2021-09-18 13:43:29 +0300

    navBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Suhaib from './suhaibComponents/suhaibMain'
-
+import Navbar from './suhaibComponents/NavBar';
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar/>
         <h1>HEllO</h1>
         <Suhaib/>
       </div>
