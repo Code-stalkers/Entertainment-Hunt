133e29b54865ec497c9b90fd566fa5a749237c7b
Yousef Obeidat <y.linux96@gmail.com>
2021-09-21 12:41:31 +0300

    deleting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import Movies from './HarounComponents/Movies';
+// import Movies from './HarounComponents/Movies';
 
 // import  {Card,Button,Col,Row,Container} from 'react-bootstrap'
 import Footer from './YousefComponents/footer'
 // import LoginButton from './YousefComponents/loginButton'
-import Suhaib from './suhaibComponents/suhaibMain'
+// import Suhaib from './suhaibComponents/suhaibMain'
 import Navbar from './YousefComponents/NavBar';
 import Home from './YousefComponents/HomePage';
 import { withAuth0 } from '@auth0/auth0-react';
@@ -28,8 +28,8 @@ class App extends Component {
           <Switch>
 
 
-            <Route exact path="/game">   <Suhaib/>  </Route>
-            <Route exact path="/movie">  <Movies/> </Route>
+            {/* <Route exact path="/game">   <Suhaib/>  </Route>
+            <Route exact path="/movie">  <Movies/> </Route> */}
             <Route exact path="/"> <Home/> </Route>
 
 
