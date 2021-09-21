db611fa34b2c638199872078849f7e71d00c5813
Yousef Obeidat <y.linux96@gmail.com>
2021-09-21 23:34:15 +0300

    nav-bar-color

--- src/YousefComponents/Header.js
+++ src/YousefComponents/Header.js
@@ -53,7 +53,7 @@
 
 
 //                                 <Nav.Link href="/">Home</Nav.Link>
-                                
+
 //                                 <NavDropdown title="Pages" id="collasible-nav-dropdown">
 //                                     <NavDropdown.Item href="/game">Games </NavDropdown.Item>
 //                                     <NavDropdown.Item href="/movie"> Movies</NavDropdown.Item>
@@ -91,30 +91,36 @@ import { MDBBtn } from 'mdb-react-ui-kit';
 export class Header extends Component {
 
     constructor(props) {
-                super(props);
-                this.state = {
-                    show: false, }}
-        
-            handleShow = () => {
-                this.setState({
-                    show: true,})}
-        
-        
-            handleClose = () => {
-                this.setState({
-                    show: false, })}
-        
+        super(props);
+        this.state = {
+            show: false,
+        }
+    }
+
+    handleShow = () => {
+        this.setState({
+            show: true,
+        })
+    }
+
+
+    handleClose = () => {
+        this.setState({
+            show: false,
+        })
+    }
+
 
 
 
 
     render() {
         return (
-            
-            <div>
-     
 
-                 {/* <nav class="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top">
+            <div>
+
+
+                {/* <nav class="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top">
                 <div class="container text-light">
                     <div class="w-100 d-flex justify-content-between">
                         <div>
@@ -133,79 +139,79 @@ export class Header extends Component {
                 </div>
             </nav> */}
 
-            <nav class="navbar navbar-expand-lg navbar-light shadow">
-                <div class="container d-flex justify-content-between align-items-center">
+                <nav style={{ backgroundColor: "#212934" }} class="navbar navbar-expand-lg navbar-light shadow">
+                    <div class="container d-flex justify-content-between align-items-center">
 
-                    <a class="navbar-brand text-success logo h1 align-self-center" href="/">
-                    Fun-Hunter
-                    </a>
+                        <a class="navbar-brand text-success logo h1 align-self-center" href="/">
+                            Fun-Hunter
+                        </a>
 
-                    <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+                        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-                    <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
-                        <div class="flex-fill">
-                            <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/game">games</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="movie">movies</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/crypto">crypto</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/profile">Profile</a> 
-                                </li> 
-                               
-                            </ul>                           
-                        </div>
-                        <div class="navbar align-self-center d-flex">
-                            <div class="d-lg-none flex-sm-fill mt-3 mb-4 col-7 col-sm-auto pr-3">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" id="inputMobileSearch" placeholder="Search ..." />
-                                    <div class="input-group-text">
-                                        <i class="fa fa-fw fa-search"></i>
+                        <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
+                            <div class="flex-fill">
+                                <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/game">games</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="movie">movies</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/crypto">crypto</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/profile">Profile</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                            <div class="navbar align-self-center d-flex">
+                                <div class="d-lg-none flex-sm-fill mt-3 mb-4 col-7 col-sm-auto pr-3">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" id="inputMobileSearch" placeholder="Search ..." />
+                                        <div class="input-group-text">
+                                            <i class="fa fa-fw fa-search"></i>
+                                        </div>
                                     </div>
                                 </div>
+                                {/* <a class="nav-icon d-none d-lg-inline" href="#" data-bs-toggle="modal" data-bs-target="#templatemo_search">
+                                    <i class="fa fa-fw fa-search text-dark mr-2"></i>
+                                </a>
+                                <a class="nav-icon position-relative text-decoration-none" href="#">
+                                    <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
+                                    <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">7</span>
+                                </a>
+                                <a class="nav-icon position-relative text-decoration-none" href="#">
+                                    <i class="fa fa-fw fa-user text-dark mr-3"></i>
+                                    <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">+99</span>
+                                </a> */}
                             </div>
-                            <a class="nav-icon d-none d-lg-inline" href="#" data-bs-toggle="modal" data-bs-target="#templatemo_search">
-                                <i class="fa fa-fw fa-search text-dark mr-2"></i>
-                            </a>
-                            <a class="nav-icon position-relative text-decoration-none" href="#">
-                                <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
-                                <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">7</span>
-                            </a>
-                            <a class="nav-icon position-relative text-decoration-none" href="#">
-                                <i class="fa fa-fw fa-user text-dark mr-3"></i>
-                                <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">+99</span>
-                            </a>
                         </div>
-                    </div>
 
-                </div>
-            </nav>
-
-            <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                    <div class="w-100 pt-1 mb-5 text-right">
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <form action="" method="get" class="modal-content modal-body border-0 p-0">
-                        <div class="input-group mb-2">
-                            <input type="text" class="form-control" id="inputModalSearch" name="q" placeholder="Search ..." />
-                            <button type="submit" class="input-group-text bg-success text-light">
-                                <i class="fa fa-fw fa-search text-white"></i>
-                            </button>
+                </nav>
+
+                <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg" role="document">
+                        <div class="w-100 pt-1 mb-5 text-right">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                    </form>
+                        <form action="" method="get" class="modal-content modal-body border-0 p-0">
+                            <div class="input-group mb-2">
+                                <input type="text" class="form-control" id="inputModalSearch" name="q" placeholder="Search ..." />
+                                <button type="submit" class="input-group-text bg-success text-light">
+                                    <i class="fa fa-fw fa-search text-white"></i>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
 
             </div>
         )
