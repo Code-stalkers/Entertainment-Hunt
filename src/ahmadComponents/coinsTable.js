88cf6c28d439918f857669b19817c9ae28c2d22a
ahmedbani <a13banisal@gmail.com>
2021-09-22 18:39:08 +0300

    styling crypto page

--- src/ahmadComponents/coinsTable.js
+++ src/ahmadComponents/coinsTable.js
@@ -3,7 +3,7 @@ import { Table } from "react-bootstrap";
 class CoinsTable extends React.Component {
   render() {
     return (
-      <>
+      <div id='tableDiv'>
         <Table responsive id='table'>
           <thead>
             <tr>
@@ -26,13 +26,29 @@ class CoinsTable extends React.Component {
                     <td>
                      <h5>{item.name}</h5> {item.symbol}
                     </td>
-                    <td>{item.quotes.USD.price} $</td>
-                    <td>{item.quotes.USD.percent_change_24h} %</td>
-                    <td>{item.quotes.USD.percent_change_7d} %</td>
+                    { item.quotes.USD.percent_change_24h<0 ?
+                   <>
+                    <td class='red'>{item.quotes.USD.price} $</td>
+                    <td class='red'>{item.quotes.USD.percent_change_24h} %</td>
+                    {item.quotes.USD.percent_change_7d<0 ?
+                    <td class='red7d'>{item.quotes.USD.percent_change_7d} %</td> :
+                    <td class='green7d'>{item.quotes.USD.percent_change_7d} %</td>
+                    }
+                   </>
+                    :
+                    <>
+                    <td className='green'>{item.quotes.USD.price} $</td>
+                    <td className='green'>{item.quotes.USD.percent_change_24h} %</td>
+                    {item.quotes.USD.percent_change_7d<0?
+                    <td className='red7d'>{item.quotes.USD.percent_change_7d} %</td>:
+                    <td className='green7d'>{item.quotes.USD.percent_change_7d} %</td>
+                    }
+                    </>
+                    }
                     <td>{item.quotes.USD.market_cap} $</td>
                     <td>{item.quotes.USD.volume_24h} $</td>
                     <td>
-                      {item.circulating_supply} {item.symbol}
+                      {item.circulating_supply} <b>{item.symbol}</b>
                     </td>
                   </tr>
                 );
@@ -43,19 +59,34 @@ class CoinsTable extends React.Component {
                 <td>
                   <h5>{this.props.searchedCoin[0].name}</h5> {this.props.searchedCoin[0].symbol}
                 </td>
-                <td>{this.props.searchedCoin[0].price} $</td>
-                <td>{this.props.searchedCoin[0].percent_change_24h} %</td>
-                <td>{this.props.searchedCoin[0].percent_change_7d} %</td>
+                { this.props.searchedCoin[0].percent_change_24h<0 ?
+                <>
+                <td class='red'>{this.props.searchedCoin[0].price} $</td>
+                <td class='red'>{this.props.searchedCoin[0].percent_change_24h} %</td>
+                {this.props.searchedCoin[0].percent_change_7d<0?
+                <td class='red7d'>{this.props.searchedCoin[0].percent_change_7d} %</td>:
+                <td class='green7d'>{this.props.searchedCoin[0].percent_change_7d} %</td>
+                }
+                </> :
+                <>
+                <td className='green'>{this.props.searchedCoin[0].price} $</td>
+                <td className='green'>{this.props.searchedCoin[0].percent_change_24h} %</td>
+                { this.props.searchedCoin[0].percent_change_7d<0 ?
+                <td className='red7d'>{this.props.searchedCoin[0].percent_change_7d} %</td> :
+                <td className='green7d'>{this.props.searchedCoin[0].percent_change_7d} %</td>
+                }
+                </>
+                }
                 <td>{this.props.searchedCoin[0].market_cap} $</td>
                 <td>{this.props.searchedCoin[0].volume_24h} $</td>
                 <td>
-                  {this.props.searchedCoin[0].max_supply} {this.props.searchedCoin[0].symbol}
+                  {this.props.searchedCoin[0].max_supply} <b>{this.props.searchedCoin[0].symbol}</b>
                 </td>
               </tr>
             )}
           </tbody>
         </Table>
-      </>
+      </div>
     );
   }
 }
